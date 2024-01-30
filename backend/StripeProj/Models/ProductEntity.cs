2c924781228474919e4b9b68ac661c188057b043
Muhammad Zulqarnain Aslam <147504664+Zulqarnain94@users.noreply.github.com>
2024-01-30 11:17:35 +0100

    FIX models

---
+++ backend/StripeProj/Models/ProductEntity.cs
@@ -0,0 +1,9 @@
+namespace StripeProj.Models;
+
+public class ProductEntity{
+  public int Id { get; set; }
+  public string Product { get; set; }
+  public long Rate { get; set; }
+  public long Quantity { get; set; }
+  public string ImagePath { get; set; }
+}
